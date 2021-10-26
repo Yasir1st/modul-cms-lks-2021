33c26b265ae4f62d584b3b73a5aac90694b9bee6
Yasir1st <yasirnakano2nd@gmail.com>
2021-10-26 15:11:58 +0800

    modul wordpress-lks 2021

---
+++ wp-content/themes/wonderful/index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="<?php echo get_theme_file_uri('style.css') ?>">
+</head>
+<body>
+
+<div class="container">
+    <div class="header">
+        <h1>Indonesiaku</h1>
+    </div>
+    <nav class="nav">
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">Places</a></li>
+            <li><a href="">Articles</a></li>
+            <li><a href="">Contact</a></li>
+        </ul>
+    </nav>
+    <div class="hero">
+        <div class="img"></div>
+        <div class="imgmini1"></div>
+        <div class="imgmini2"></div>
+    </div>
+    <div class="main">
+        <h2>ABOUT INDONESIA</h2>
+        <p>Indonesia adalah negara kepulauan terbesar di dunia. Indonesia memiliki lebih dari 17.000 pulau, di mana hanya sekitar 7.000 pulau yang berpenghuni. Kalimantan, Jawa, Sulawesi, Sumatra dan Papua merupakan pulau utama di Indonesia. Selain itu Indonesia juga memiliki pulau-pulau kecil seperti Bali, Karimunjawa, Gili dan Lombok yang merupakan tujuan wisata lokal maupun internasional. Ibukota negara Indonesia adalah Jakarta, yang terletak di Pulau Jawa.</p>
+        <p>Dilihat dari segi geografis, kepulauan Indonesia terletak antara 5° 54′ 08″ bujur utara hingga 11° 08′ 20″ bujur selatan dan 95°00’38“ sampai 141°01’12“ bujur timur. Beberapa pulau terletak di garis ekuator. Karena itu, siang dan malam memiliki waktu yang hampir sama, yaitu 12 jam. Atas dasar letak geografis yang luas, wilayah Indonesia dibagi menjadi 3 zona waktu yaitu WIB (Waktu Indonesia Barat), WITA (Waktu Indonesia Tengah) dan WIT (Waktu Indonesia Timur). Dari satu pulau ke pulau lainnya dapat terjadi perbedaan waktu hingga 8 jam.</p>
+        <p>Selain pulau-pulau yang indah, iklim tropis yang dimiliki Indonesia juga menjadikan Indonesia menjadi tujuan wisata yang utama. Waktu terbaik untuk berwisata ke Indonesia adalah saat musim panas yang berlangsung mulai bulan April hingga Oktober. Bulan Maret dan November merupakan pergantian musim. Pada pergantian musim, cuaca di Indonesia dapat menjadi tidak menentu. Hujan, panas matahari dan angin lebat dapat datang bersamaan dalam satu hari. Sementara itu, musim hujan biasanya berlangsung mulai bulan Desember hingga Maret.</p>
+        <p>“Bhinneka Tunggal Ika” yang bermakna “beraneka ragam tetapi satu” merupakan logo nasional Repulik Indonesia. Logo ini menggambarkan masyarakat Indonesia yang majemuk namun tetap satu, juga menjadi pegangan hidup masyarakat Indonesia. Hingga tahun 2016, warga negara Indonesia diperkirakan mencapai 250 juta jiwa. Indonesia adalah negara dengan jumlah penduduk terbesar ke-4 di dunia setelah Cina, India dan Amerika (Sumber: Statista.de, 2016). Masyarakat Indonesia terdiri dari sekitar 300 suku, seperti suku Jawa, Sunda, Batak, Cina, Dayak dan Papua. Setiap suku memiliki dialek tersendiri. Secara keseluruhan terdapat lebih dari 360 dialek yang memperkaya budaya Indonesia. Namun demikian “Bahasa Indonesia” adalah bahasa nasional yang juga merupakan pemersatu bangsa Indonesia. Bahasa Inggris yang merupakan bahasa internasional juga merupakan bahasa yang sering digunakan di Indonesia. Bahasa Inggris masuk dalam kurikulum sekolah dasar di Indonesia dan merupakan bahasa bisnis. Selain itu, Indonesia juga memiliki kemajemukan dalam kehidupan beragama. Sebagian besar penduduk Indonesia memeluk agama Islam. Kurang dari 10 % masyarakat Indonesia terbagi dalam beberapa kelompok agama seperti Katolik Roma, Kristen, Hindu dan Budha. Sebagian kecil masyarakat Indonesia juga masih memeluk agama tradisional seperti misalnya kejawen yang sering ditemui di Pulau Jawa.</p>
+    </div>
+    <div class="main2">
+        <h2>BEST PLACES</h2>
+        <div class="wrap">
+            <div class="box">
+                <div class="gambar"></div>
+                <h3>Bali</h3>
+                <p>Bali Island is a small beautiful island and a part of Indonesia archipelago, and the most famous of Indonesian tourism in the world. It owns the panorama and unique culture that make this island is exclusive than others. Furthermore, It’s location is in the tropical situation as Dream Island for a vacation. Bali Island has many places of interest such as rice paddies, beautiful panorama, volcanoes, tourism activities as well as attractions. Also, it also has beautiful jungle, long sandy beaches, warm blue water, crashing surf and friendly people. Moreover, the local people present daily community ritual and a lot of things make your holiday unforgettable. In Bali, the spirits are coming out to play in the moonlight. You can discover a festival and even a funeral on the island. Meanwhile, a good time of the day with a sea breeze will complete your holiday dream.</p>
+            </div>
+            <div class="box">
+                <div class="gambar2"></div>
+                <h3>Nusa Tenggara Timur</h3>
+                <p>Bali Island is a small beautiful island and a part of Indonesia archipelago, and the most famous of Indonesian tourism in the world. It owns the panorama and unique culture that make this island is exclusive than others. Furthermore, It’s location is in the tropical situation as Dream Island for a vacation. Bali Island has many places of interest such as rice paddies, beautiful panorama, volcanoes, tourism activities as well as attractions. Also, it also has beautiful jungle, long sandy beaches, warm blue water, crashing surf and friendly people. Moreover, the local people present daily community ritual and a lot of things make your holiday unforgettable. In Bali, the spirits are coming out to play in the moonlight. You can discover a festival and even a funeral on the island. Meanwhile, a good time of the day with a sea breeze will complete your holiday dream.</p>
+            </div>
+            <div class="box">
+                <div class="gambar3"></div>
+                <h3>Jakarta</h3>
+                <p>Bali Island is a small beautiful island and a part of Indonesia archipelago, and the most famous of Indonesian tourism in the world. It owns the panorama and unique culture that make this island is exclusive than others. Furthermore, It’s location is in the tropical situation as Dream Island for a vacation. Bali Island has many places of interest such as rice paddies, beautiful panorama, volcanoes, tourism activities as well as attractions. Also, it also has beautiful jungle, long sandy beaches, warm blue water, crashing surf and friendly people. Moreover, the local people present daily community ritual and a lot of things make your holiday unforgettable. In Bali, the spirits are coming out to play in the moonlight. You can discover a festival and even a funeral on the island. Meanwhile, a good time of the day with a sea breeze will complete your holiday dream.</p>
+            </div>
+            <div class="box">
+                <div class="gambar4"></div>
+                <h3>Papua</h3>
+                <p>Bali Island is a small beautiful island and a part of Indonesia archipelago, and the most famous of Indonesian tourism in the world. It owns the panorama and unique culture that make this island is exclusive than others. Furthermore, It’s location is in the tropical situation as Dream Island for a vacation. Bali Island has many places of interest such as rice paddies, beautiful panorama, volcanoes, tourism activities as well as attractions. Also, it also has beautiful jungle, long sandy beaches, warm blue water, crashing surf and friendly people. Moreover, the local people present daily community ritual and a lot of things make your holiday unforgettable. In Bali, the spirits are coming out to play in the moonlight. You can discover a festival and even a funeral on the island. Meanwhile, a good time of the day with a sea breeze will complete your holiday dream.</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="footer">
+        <h4>Copyright © 2021 - All rights reserved</h4>
+    </div>
+</div>
+    
+</body>
+</html>
